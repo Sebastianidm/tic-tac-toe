b94df10a9bd3799ad95b5b9cf94ac4a9ec7c4d1e
Sebastianidm <sebastiandiazmiranda@gmail.com>
2023-11-30 21:49:34 -0300

    Agregando funcionalidad single-player

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,38 @@ function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState(null);
+  const [isMachineTurn, setIsMachineTurn] = useState(false);
 
   useEffect(() => {
-    if (!winner) {
-      calculateWinner(board);
+    if (!winner && isMachineTurn) {
+      const availableMoves = board.reduce((acc, value, index) => {
+        if (!value) {
+          acc.push(index);
+        }
+        return acc;
+      }, []);
+  
+      const randomIndex = Math.floor(Math.random() * availableMoves.length);
+      const machineMove = availableMoves[randomIndex];
+  
+      const newBoard = board.slice();
+      newBoard[machineMove] = xIsNext ? "X" : "O";
+  
+      setBoard(newBoard);
+      setXIsNext(!xIsNext);
+      setIsMachineTurn(false);
     }
-  }, [board, winner]);
+  }, [board, isMachineTurn, winner, xIsNext]);
 
   const handleClick = (index) => {
     const newBoard = board.slice();
-    if (calculateWinner(newBoard) || newBoard[index]) {
+    if (calculateWinner(newBoard) || newBoard[index] || isMachineTurn) {
       return;
     }
     newBoard[index] = xIsNext ? "X" : "O";
     setBoard(newBoard);
     setXIsNext(!xIsNext);
+    setIsMachineTurn(true);
   };
 
   const calculateWinner = (squares) => {
@@ -46,28 +63,13 @@ function App() {
     return false;
   };
 
-  const renderSquare = (index) => {
-    const isWinnerSquare = winner && winner.line.includes(index);
-
-    return (
-      <button
-        className={`bg-red-500 border border-yellow-100 text-3xl p-2 sm:p-4 w-16 sm:w-24 h-16 sm:h-24 transition-colors hover:bg-yellow-400 ${
-          isWinnerSquare ? "text-white font-bold" : ""
-        }`}
-        onClick={() => handleClick(index)}
-      >
-        {board[index]}
-      </button>
-    );
-  };
-
   const renderBoard = () => {
     const boardRows = [];
     for (let i = 0; i < 3; i++) {
       const row = [];
       for (let j = 0; j < 3; j++) {
         const index = i * 3 + j;
-        row.push(renderSquare(index));
+        row.push(renderSquare(index, i, j));
       }
       boardRows.push(
         <div key={i} className="flex justify-center">
@@ -79,6 +81,22 @@ function App() {
       <div className="flex flex-col items-center text-yellow-100 mb-4">
         {boardRows}
       </div>
+    );
+  };
+  
+  const renderSquare = (index, i, j) => {
+    const isWinnerSquare = winner && winner.line.includes(index);
+  
+    return (
+      <button
+        key={j} 
+        className={`bg-red-500 border border-yellow-100 text-3xl p-2 sm:p-4 w-16 sm:w-24 h-16 sm:h-24 transition-colors hover:bg-yellow-400 ${
+          isWinnerSquare ? "text-white font-bold" : ""
+        }`}
+        onClick={() => handleClick(index)}
+      >
+        {board[index]}
+      </button>
     );
   };
 
@@ -102,7 +120,7 @@ function App() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-yellow-100">
-      <div className="bg-red-500 rounded-lg shadow-md max-w-md w-full overflow-hidden p-4 mt-8 mb-8 flex flex-col items-center">
+      <div className="bg-red-500 rounded-lg shadow-md max-w-lg w-full overflow-hidden p-4 mx-3 mt-8 mb-8 flex flex-col items-center">
         <h1 className="text-4xl text-yellow-100 mb-4 text-center font-custom">
           Tic-Tac-Toe
         </h1>
